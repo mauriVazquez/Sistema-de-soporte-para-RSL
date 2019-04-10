dc90c2f3fd96d243bfd5cfc772add326b39e8aa0
mauriVazquez <32914450+mauriVazquez@users.noreply.github.com>
2019-04-10 20:09:53 -0300

    primeros models
    primeros modelos

--- SoporteRSL/Investigadores/models.py
+++ SoporteRSL/Investigadores/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Investigador(models.Model):
+    nombre = models.CharField()
+    apellido = models.CharField()
+    email = models.EmailField()
+    
+    def __str__(self):
+        return '%s %s' % (self.nombre, self.apellido)
--- SoporteRSL/Revisiones/models.py
+++ SoporteRSL/Revisiones/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from Investigadores import models
 
 # Create your models here.
+
+class PreguntaDeInvestigacion(models.Model):
+    pregunta = models.CharField(max_length=150)
+    revision = models.ForeignKey('Revision', on_delete=models.CASCADE)
+
+
+class Revision(models.Model):
+    meta_de_necesidad_de_informacion = models.Textfield()
+    investigadores = models.ManyToManyField('Investigador',)
+    cadena_de_busqueda = models.
+
