2e0a756e0bec8d0793fd6cf1c49fd1ea24082031
Mauricio Vazquez <32914450+mauriVazquez@users.noreply.github.com>
2019-04-11 17:39:58 -0300

    models
    modelo de revision

--- SoporteRSL/Revisiones/models.py
+++ SoporteRSL/Revisiones/models.py
@@ -8,9 +8,31 @@ class PreguntaDeInvestigacion(models.Model):
     revision = models.ForeignKey('Revision', on_delete=models.CASCADE)
 
 
+class Metadato(models.Model):
+    nombre = models.CharField(max_length=50)
+
+
+class Biblioteca(models.Model):
+    nombre = models.CharField(max_length=50)
+
+
+class Criterio(models.Model):
+
+    TIPO_DE_CRITERIO_CHOICE=(
+        ('IN', 'inclusion'),
+        ('EX', 'exclusion'),
+        ('CA', 'calidad')
+    )
+
+    tipo = models.CharField(max_length=2, choices=TIPO_DE_CRITERIO_CHOICE, default='inclusion')
+    descripcion = models.CharField(max_length=200)
+    revison = models.ForeignKey("Revision", on_delete=models.CASCADE)
+
+
 class Revision(models.Model):
     meta_de_necesidad_de_informacion = models.Textfield()
-    investigadores = models.ManyToManyField('Investigador',)
+    investigadores = models.ManyToManyField('Investigador')
     cadena_de_busqueda = models.Charfield(max_length=200,)
-
-
+    metadatos = models.ManyToManyField("Metadato")
+    bibliotecas = models.ManyToManyField("Biblioteca")
+    prueba_piloto = models.BooleanField(default=False)
